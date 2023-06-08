f73d33ee04e4f4127cf9f7cf34beffcd8bbf3e2f
Vee-Git <veejarytv@gmail.com>
2023-06-08 15:26:07 +0200

    Update LijnDetectie.h
    Maar ook echt een extreme skill issue

--- Huidiglibrary/LijnDetectie.h
+++ Huidiglibrary/LijnDetectie.h
@@ -1,13 +1,23 @@
-#include <Zumo32U4.h> 
-#include <Zumo32U4LineSensors.h>
+#ifndef ZUMO_H
+#define ZUMO_H
 
-using namespace std;
+#include <Zumo32U4.h>
+#include <Wire.h>
 
-class LijnDet
- {
- public:
-    int kleurHerkenning();
-    void starter();
-    void calibreren();
-    void zelfRijden();
- };
+class Zumo {
+public:
+  Zumo();
+  void setup();
+  void loop();
+  void kleurHerkenning();
+  
+private:
+  Zumo32U4ButtonA buttonA;
+  Zumo32U4LineSensors lijnSensor;
+  Zumo32U4Motors motors;
+
+  void SensCali();
+  void shRead();
+};
+
+#endif
