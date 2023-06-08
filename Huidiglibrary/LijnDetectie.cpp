d299d4c007284495d4cb24d966a70d80fcac2c44
Vee-Git <veejarytv@gmail.com>
2023-06-08 15:25:34 +0200

    Update LijnDetectie.cpp
    Jeev had een skill issue

--- Huidiglibrary/LijnDetectie.cpp
+++ Huidiglibrary/LijnDetectie.cpp
@@ -1,24 +1,35 @@
-#include <LijnDetectie.h>
-#include <Zumo32U4.h>
-#include <Wire.h>
+#include "LijnDetectie.h"
 
-Zumo32U4LineSensors lol;
-Zumo32U4Motors motors;
-
-using namespace std;
 #define NUM_SENSORS 5
-uint16_t lineSensorValues[NUM_SENSORS];
+unsigned int lineSensorValues[NUM_SENSORS];
 
-const uint16_t maxSnelheid = 200;
+const uint16_t maxSnelheid = 300;
 int16_t laatsteError = 0;
 
-void LijnDet::starter() {
-    lol.initFiveSensors();
-    lol.readCalibrated(lineSensorValues);
-};
+Zumo::Zumo() {
+  // Constructor
+}
 
-LijnDet::kleurHerkenning() {
-    lol.read(lineSensorValues);
+void Zumo::SensCali() {
+  for (uint16_t i = 0; i < 120; i++) {
+    if (i > 30 && i <= 90) {
+      motors.setSpeeds(-200, 200);
+    } else {
+      motors.setSpeeds(200, -200);
+    }
+    lijnSensor.calibrate();
+  }
+  motors.setSpeeds(0, 0);
+}
+
+void Zumo::shRead() {
+  while (!buttonA.getSingleDebouncedPress()) {
+    lijnSensor.readCalibrated(lineSensorValues);
+  }
+}
+
+void Zumo::kleurHerkenning() {
+    lijnSensor.read(lineSensorValues);
     const int a = lineSensorValues[1];
     const int b = lineSensorValues[2];
     const int c = lineSensorValues[3];
@@ -38,37 +49,27 @@ LijnDet::kleurHerkenning() {
     }
 };
 
-
-void LijnDet::calibreren(){
-  for (uint16_t i = 0; i < 120; i++){
-    if(i > 30 && i <= 90){
-      motors.setSpeeds(-200,200);
-    }else{
-      motors.setSpeeds(200,-200);
-    }
-    lol.calibrate();
-  }
-  motors.setSpeeds(0,0);
+void Zumo::setup() {
+  lijnSensor.initFiveSensors();
+  buttonA.waitForButton();
+  SensCali();
+  shRead();
 }
 
-void LijnDet::zelfRijden() {
-  // put your main code here, to run repeatedly:
-  int16_t positie = lol.readLine(lineSensorValues);
-  //error
+void Zumo::loop() {
+  int16_t positie = lijnSensor.readLine(lineSensorValues);
+
   int16_t error = positie - 1000;
-  // verschilspeed
+
   int16_t snelheidVerschil = error / 4 + 6 * (error - laatsteError);
 
   laatsteError = error;
 
-  //bepaal wnr de robot naar links en rechts draait 
   int16_t linksSpeed = (int16_t)maxSnelheid + snelheidVerschil;
   int16_t rechtsSpeed = (int16_t)maxSnelheid - snelheidVerschil;
 
   linksSpeed = constrain(linksSpeed, 0, (int16_t)maxSnelheid);
-  rechtsSpeed = constrain(rechtsSpeed,0, (int16_t)maxSnelheid);
+  rechtsSpeed = constrain(rechtsSpeed, 0, (int16_t)maxSnelheid);
 
   motors.setSpeeds(linksSpeed, rechtsSpeed);
 }
-
-// lol
