5f58cd8f87bd3b415907ad26c8ea8e19caf895b3
Vee-Git <veejarytv@gmail.com>
2023-06-14 00:18:18 +0200

    Update LijnDetectie.cpp

--- Huidiglibrary/LijnDetectie.cpp
+++ Huidiglibrary/LijnDetectie.cpp
@@ -1,13 +1,16 @@
 #include "LijnDetectie.h"
+#include "KleurHerkenning.h"
 
 #define NUM_SENSORS 5
 uint16_t lineSensorValues[NUM_SENSORS];
 
+int maxSnel = 0;
+
 Zumo32U4ButtonA buttonA;
 Zumo32U4LineSensors lijnSensor;
 Zumo32U4Motors motors;
+KleurHerkenning kl;
 
-const uint16_t maxSnelheid = 300;
 int16_t laatsteError = 0;
 
 LijnHerkenning::LijnHerkenning() {
@@ -40,19 +43,36 @@ void LijnHerkenning::calibratie() {
 }
 
 void LijnHerkenning::volgen() {
+  // lijnSensor.read(lineSensorValues);
+  // const int a = lineSensorValues[1];
+  // const int b = lineSensorValues[2];
+  // const int c = lineSensorValues[3];
+  // const int gem = (a+b+c);
+  kl.leesWaardes();
+  bool f = kl.groen();
+
   int16_t positie = lijnSensor.readLine(lineSensorValues);
   Serial.println(positie);
   int16_t error = positie - 1100;
 
   int16_t snelheidVerschil = error / 4 + 6 * (error - laatsteError);
 
+  Serial.println(f);
+
+  if (f) {
+    maxSnel = 150;
+  }
+  else {
+    maxSnel = 300;
+  }
+
   laatsteError = error;
 
-  int16_t linksSpeed = (int16_t)maxSnelheid + snelheidVerschil;
-  int16_t rechtsSpeed = (int16_t)maxSnelheid - snelheidVerschil;
+  int16_t linksSpeed = maxSnel + snelheidVerschil;
+  int16_t rechtsSpeed = maxSnel - snelheidVerschil;
 
-  linksSpeed = constrain(linksSpeed, -250, (int16_t)maxSnelheid);
-  rechtsSpeed = constrain(rechtsSpeed, -250, (int16_t)maxSnelheid);
+  linksSpeed = constrain(linksSpeed, -250, maxSnel);
+  rechtsSpeed = constrain(rechtsSpeed, -250, maxSnel);
 
   motors.setSpeeds(linksSpeed, rechtsSpeed);
 }
