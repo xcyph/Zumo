37e29b1e64fd9b6f7b486a63de02038c9c83c1c8
JesseStarmans <131788688+JesseStarmans@users.noreply.github.com>
2023-06-16 13:49:18 +0200

    Update Gyro testfile.ino

--- Ino's/Gyro testfile.ino
+++ Ino's/Gyro testfile.ino
@@ -21,7 +21,7 @@ void loop() {
   Balanceren.correctWithAccelAngle();
   //Balanceren.printAngles();
 
-  if (Balanceren.testAngle(-84)) {    //Als de Zumo horizontaal staat met de batterijen naar beneden, heeft dit een Gyro-angle van -90
+  if (Balanceren.testAngle(-84, -40)) {    //Als de Zumo horizontaal staat met de batterijen naar beneden, heeft dit een Gyro-angle van -90. Ik heb in deze testfile -84 als waarde gebruikt om te zorgen dat de Zumo niet de hele tijd weg probeert te rijden.
     rijden.vooruit(200);
   }
   else {
