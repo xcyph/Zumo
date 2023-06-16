592d9aa41c94d131b530545ad652c047efda802b
JesseStarmans <131788688+JesseStarmans@users.noreply.github.com>
2023-06-16 11:57:26 +0200

    Add files via upload

---
+++ Ino's/Eigen Gyro Verkortte testfile.ino
@@ -0,0 +1,31 @@
+#include <Besturing.h>
+#include <Gyro.h>
+
+Balanceren Balanceren;
+motoren rijden;
+Zumo32U4ButtonA buttonA;
+
+void setup() {
+  Balanceren.init();
+  Balanceren.averageYOffset();
+  
+  while(!buttonA.getSingleDebouncedPress()) {
+    Balanceren.updateGyroAngle();    
+    Balanceren.correctWithAccelAngle();
+  }
+  
+}
+
+void loop() {
+  Balanceren.updateGyroAngle();
+  Balanceren.correctWithAccelAngle();
+  //Balanceren.printAngles();
+
+  if (Balanceren.testAngle(-84)) {    //Als de Zumo horizontaal staat met de batterijen naar beneden, heeft dit een Gyro-angle van -90
+    rijden.vooruit(200);
+  }
+  else {
+    rijden.stop();
+  }
+
+}
