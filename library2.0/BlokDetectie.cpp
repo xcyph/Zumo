3d6509d3108bd030997ab1dfaedb80b76dcd0a88
marijnvdwekken <50845116+marijnvdwekken@users.noreply.github.com>
2023-06-16 15:59:05 +0200

    Update BlokDetectie.cpp

--- library2.0/BlokDetectie.cpp
+++ library2.0/BlokDetectie.cpp
@@ -1,51 +1,61 @@
 #include <BlokDetectie.h>
 
-BlokDetectie :: BlokDetectie (): leftValue(0), rightValue(0), gevonden(false)   //constructor van blokdetectie
-{ 
-
-}
-
-BlokDetectie :: ~BlokDetectie()                                                 //destructor van blokdetectie
+BlokDetectie::BlokDetectie(): leftValue(0), rightValue(0), gevonden(false)
 {
-
+    // Constructor van BlokDetectie
+    // Initialiseert de objectvariabelen
 }
 
-
-void BlokDetectie :: init() {                                                   //hier wordt de proximitysensor geinitialiseerd                                            
-prox.initFrontSensor();
-
-}
-
-void BlokDetectie :: read()                                                     //hier worden de waardes vanuit de linker en rechter gelezen
+BlokDetectie::~BlokDetectie()
 {
-  prox.read();
-  leftValue = prox.countsFrontWithLeftLeds();
-  rightValue = prox.countsFrontWithRightLeds();
+    // Destructor van BlokDetectie
+    // Wordt gebruikt om geheugen op te ruimen of bronnen vrij te geven
 }
 
-void BlokDetectie :: rijden(){                                                  //deze methode zorgt er voor dat de zumo achter het blokje gaat
-   
-
-    if (gevonden == false) 
+void BlokDetectie::init()
 {
-    motorsblok.setMotorSpeed(200,-200);
-}
-    if ( (leftValue + rightValue) >= 9 )
-{
-    gevonden = true; 
-    motorsblok.setMotorSpeed(300,300);
-
-}
-else {
-    gevonden = false;
-}
-    if (leftValue > rightValue) 
-{
-    motorsblok.setMotorSpeed(300, 350);
-}
-    else if (rightValue > leftValue) 
-{
-    motorsblok.setMotorSpeed(350, 300);
+    // Initialiseert de proximitysensor
+    prox.initFrontSensor();
 }
 
+void BlokDetectie::read()
+{
+    // Leest de waardes van de linker- en rechterzijde
+    prox.read();
+    leftValue = prox.countsFrontWithLeftLeds();
+    rightValue = prox.countsFrontWithRightLeds();
+}
+
+void BlokDetectie::rijden()
+{
+    // Methode om de Zumo achter het blokje te laten rijden
+
+    if (gevonden == false)
+    {
+        // Als het blokje nog niet is gevonden, draai naar links
+        motorsblok.setMotorSpeed(200, -200);
+    }
+
+    if ((leftValue + rightValue) >= 9)
+    {
+        // Als de som van linker- en rechterwaarde groter of gelijk aan 9 is, is het blokje gevonden
+        gevonden = true;
+        motorsblok.setMotorSpeed(300, 300);
+    }
+    else
+    {
+        // Reset gevonden naar false als het blokje niet is gevonden
+        gevonden = false;
+    }
+
+    if (leftValue > rightValue)
+    {
+        // Als linkerwaarde groter is dan rechterwaarde, draai naar rechts
+        motorsblok.setMotorSpeed(300, 350);
+    }
+    else if (rightValue > leftValue)
+    {
+        // Als rechterwaarde groter is dan linkerwaarde, draai naar links
+        motorsblok.setMotorSpeed(350, 300);
+    }
 }
