fd19a225dcdc9c136a0debdf9c1891eab94cda7d
JesseStarmans <131788688+JesseStarmans@users.noreply.github.com>
2023-06-16 13:15:32 +0200

    Update BlokDetectie.h

--- library2.0/BlokDetectie.h
+++ library2.0/BlokDetectie.h
@@ -10,16 +10,18 @@ class BlokDetectie : public Zumo32U4Motors, public Zumo32U4ProximitySensors
 { 
 public: 
 
-BlokDetectie();         //constructor klasse BlokDetectie
-~BlokDetectie();        //destructor klasse BlokDetectie 
-void init();            //initializeren van de proximity sensor
-void read();            //het lezen van de waardes vanuit de proximitysensors
-void rijden();          //wordt er nog uit gegooid 
+BlokDetectie();                 //constructor klasse BlokDetectie
+~BlokDetectie();                //destructor klasse BlokDetectie 
+void init();                    //initializeren van de proximity sensor
+void read();                    //het lezen van de waardes vanuit de proximitysensors
+void rijden();                  //wordt er nog uit gegooid 
 
 private: 
-uint8_t leftValue;      //wanneer de sensor iets ziet op links, dan gaat deze waarde omhoog
-uint8_t rightValue;     //wanneer de sensor iets ziet op rechts, dan gaat deze waarde omhoog
-bool gevonden;          //wanneer de sensor iets gevonden heb dan gaat "gevonden" op true
+uint8_t leftValue;              //wanneer de sensor iets ziet op links, dan gaat deze waarde omhoog
+uint8_t rightValue;             //wanneer de sensor iets ziet op rechts, dan gaat deze waarde omhoog
+bool gevonden;                  //wanneer de sensor iets gevonden heb dan gaat "gevonden" op true
+motoren motorsblok;             //het object motors wordt gemaakt
+Zumo32U4ProximitySensors prox;  //het object prox wordt gemaakt
 
 
 
