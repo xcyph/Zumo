8edc64aafd6802f4d44f008355d2cce5362eabf9
JesseStarmans <131788688+JesseStarmans@users.noreply.github.com>
2023-06-16 11:46:10 +0200

    Add files via upload

---
+++ Ino's/Eigen XBEE code test.ino
@@ -0,0 +1,37 @@
+#include <Besturing.h>
+#include <XBEE.h>
+
+XBEE XBEE1(9600);
+motoren rijden;
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (XBEE1.available()) {
+    char ontvangen = XBEE1.read();
+    if(ontvangen == 'w') {
+      rijden.vooruit(200);
+    }
+    else if (ontvangen == 'a') {
+      rijden.linksomDraaien(200);
+    }
+    else if (ontvangen == 's') {
+      rijden.achteruit(200);
+    }
+    else if (ontvangen == 'd') {
+      rijden.rechtsomDraaien(200);
+    }
+    else if (ontvangen == 'q') {
+      rijden.setMotorSpeed(100, 200);
+    }
+    else if (ontvangen == 'e') {
+      rijden.setMotorSpeed(200, 100);
+    }
+    else {
+      rijden.stop();
+    }
+  }
+}
