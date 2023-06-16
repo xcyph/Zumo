228dac3eda4385835a73bbb98f0f8648ff87e465
marijnvdwekken <50845116+marijnvdwekken@users.noreply.github.com>
2023-06-16 16:04:12 +0200

    Update Buttons.h

--- library2.0/Buttons.h
+++ library2.0/Buttons.h
@@ -1,33 +1,25 @@
-
 #include <Pushbutton.h>
 #include <FastGPIO.h>
 #include <USBPause.h>
 
-#define ZUMO_32U4_BUTTON_A 14              //de knoppen worden hier aan een pin gekoppeld
+#define ZUMO_32U4_BUTTON_A 14              // De knoppen worden hier aan een pin gekoppeld
 #define ZUMO_32U4_BUTTON_B IO_D5
 #define ZUMO_32U4_BUTTON_C 17
 
-
-
-class ButtonA : public PushbuttonBase       //dit is de klasse van de knop A 
+class ButtonA : public PushbuttonBase       // Dit is de klasse voor knop A
 { 
 public: 
-virtual bool isPressed();                  
-
+    virtual bool isPressed();                   // Methode om te controleren of knop A is ingedrukt
 };
 
-class ButtonB : public PushbuttonBase       //dit is de klasse van de knop B
+class ButtonB : public PushbuttonBase       // Dit is de klasse voor knop B
 {
 public:
-virtual bool isPressed();
-  
+    virtual bool isPressed();                 // Methode om te controleren of knop B is ingedrukt
 };
 
-class ButtonC : public PushbuttonBase       //dit is de klasse van de knop C
+class ButtonC : public PushbuttonBase       // Dit is de klasse voor knop C
 {
 public:
-virtual bool isPressed();
+    virtual bool isPressed();                 // Methode om te controleren of knop C is ingedrukt
 };
-
-
-
